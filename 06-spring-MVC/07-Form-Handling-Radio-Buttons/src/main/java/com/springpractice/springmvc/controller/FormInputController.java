e586213f3c62712dce0121be5c2b8d32b075ce97
Zeyad Ayman <70069422+zeyadayman99@users.noreply.github.com>
2025-10-25 23:00:55 +0300

    adding new spring mvc exercise

---
+++ 06-spring-MVC/07-Form-Handling-Radio-Buttons/src/main/java/com/springpractice/springmvc/controller/FormInputController.java
@@ -0,0 +1,35 @@
+package com.springpractice.springmvc.controller;
+
+import com.springpractice.springmvc.model.Student;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class FormInputController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${languages}")
+    private List<String> languages;
+
+    //Controller Method to Show initial Form
+    @GetMapping("/showForm")
+    public String handleFormInput(Model model) {
+        model.addAttribute("student", new Student());
+        model.addAttribute("countries", countries);
+        model.addAttribute("languages", languages);
+        return "userInputForm";
+    }
+
+    @PostMapping("/processForm")
+    public String processFormInput(@ModelAttribute("student") Student student, Model model) {
+        return "process-user-info";
+    }
+}
---
+++ 06-spring-MVC/07-Form-Handling-Radio-Buttons/src/main/java/com/springpractice/springmvc/model/Student.java
@@ -0,0 +1,17 @@
+package com.springpractice.springmvc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String country;
+    private String progLanguage;
+}
